5f13d5514bc5f99fb000b925a9081f0f74c3af6a
harley9293 <xuhanlin2012@gmail.com>
2023-07-25 16:32:06 +0800

    echo example

--- example/echo/echo.go
+++ example/echo/echo.go
@@ -1,6 +1,9 @@
 package echo
 
-import "github.com/harley9293/nebulus/pkg/def"
+import (
+	"github.com/harley9293/nebulus"
+	"github.com/harley9293/nebulus/pkg/def"
+)
 
 type Service struct {
 	def.DefaultHandler
@@ -8,4 +11,12 @@ type Service struct {
 
 func (m *Service) Print(req string) string {
 	return "echo: " + req
+}
+
+func Example() {
+	err := nebulus.Register("echo", new(Service))
+	if err != nil {
+		panic(err)
+	}
+	nebulus.Run()
 }
